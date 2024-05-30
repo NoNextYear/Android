32a22adbf7d504c6f60e171af8f5b1103ec2bea5
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-05-30 23:44:40 +0900

    feat : 그룹 만들기 액션 추가

--- app/src/main/java/com/example/myapplication/fragments/FriendFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/FriendFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.adapter.FriendAdapter
 import com.example.myapplication.databinding.FragmentFriendBinding
@@ -31,18 +30,25 @@ class FriendFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Initialize your friend list here
         friendList = listOf(
             Friend("가나다", R.drawable.ic_circle),
             Friend("가다나", R.drawable.ic_circle),
             Friend("다나카", R.drawable.ic_circle),
             Friend("다카나", R.drawable.ic_circle),
             Friend("다나키", R.drawable.ic_circle),
+            Friend("나성범", R.drawable.ic_circle),
+            Friend("박찬호", R.drawable.ic_circle),
+            Friend("박지성", R.drawable.ic_circle),
+            Friend("이청용", R.drawable.ic_circle),
+            Friend("김덕배", R.drawable.ic_circle),
             Friend("손흥민", R.drawable.ic_circle),
             Friend("이강인", R.drawable.ic_circle)
         )
 
-        friendAdapter = FriendAdapter(friendList)
+        friendAdapter = FriendAdapter(friendList) { isAnyFriendSelected ->
+            binding.createGroupButton.visibility = if (isAnyFriendSelected) View.VISIBLE else View.GONE
+        }
+
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
         binding.recyclerView.adapter = friendAdapter
     }
