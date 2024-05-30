639f20b5990cfaf83b46afe64bcbf2fd7eefcc57
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-05-30 21:54:53 +0900

    feat : 친구 선택 액션 추가

--- app/src/main/java/com/example/myapplication/adapter/FriendAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/FriendAdapter.kt
@@ -1,7 +1,7 @@
 package com.example.myapplication.adapter
 
 import android.graphics.PorterDuff
-import android.graphics.drawable.LayerDrawable
+import android.graphics.drawable.VectorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -44,17 +44,17 @@ class FriendAdapter(private val friends: List<Friend>) :
 
         fun bind(friend: Friend) {
             binding.friendName.text = friend.name
-            val drawable = binding.friendImage.drawable
-            if (drawable is LayerDrawable) {
-                val fillDrawable = drawable.findDrawableByLayerId(R.id.fill)
-                if (friend.isSelected) {
-                    fillDrawable.setColorFilter(ContextCompat.getColor(binding.root.context, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
-                    binding.checkImage.visibility = View.VISIBLE
-                } else {
-                    fillDrawable.setColorFilter(ContextCompat.getColor(binding.root.context, android.R.color.transparent), PorterDuff.Mode.SRC_IN)
-                    binding.checkImage.visibility = View.GONE
-                }
+            val drawable = binding.friendImage.drawable as VectorDrawable
+
+            // strokeWidth를 설정하는 대신 ColorFilter를 사용하여 테두리 색상 변경
+            if (friend.isSelected) {
+                drawable.setColorFilter(ContextCompat.getColor(binding.root.context, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
+                binding.checkImage.visibility = View.VISIBLE
+            } else {
+                drawable.setColorFilter(ContextCompat.getColor(binding.root.context, android.R.color.black), PorterDuff.Mode.SRC_IN)
+                binding.checkImage.visibility = View.GONE
             }
+            binding.friendImage.setImageDrawable(drawable)
 
             if (friend.isFavorite) {
                 binding.favoriteIcon.setColorFilter(ContextCompat.getColor(binding.root.context, android.R.color.holo_orange_light), PorterDuff.Mode.SRC_IN)
