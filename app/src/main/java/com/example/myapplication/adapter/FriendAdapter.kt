288e1a1b381519231326aaa2c0c8d1651a6b8731
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-05-30 21:45:16 +0900

    feat : ic_circle복구

--- app/src/main/java/com/example/myapplication/adapter/FriendAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapter/FriendAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.adapter
 
 import android.graphics.PorterDuff
+import android.graphics.drawable.LayerDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,16 +34,33 @@ class FriendAdapter(private val friends: List<Friend>) :
                 friend.isSelected = !friend.isSelected
                 notifyItemChanged(adapterPosition)
             }
+
+            binding.favoriteIcon.setOnClickListener {
+                val friend = friends[adapterPosition]
+                friend.isFavorite = !friend.isFavorite
+                notifyItemChanged(adapterPosition)
+            }
         }
 
         fun bind(friend: Friend) {
             binding.friendName.text = friend.name
-            if (friend.isSelected) {
-                binding.friendImage.setColorFilter(ContextCompat.getColor(binding.root.context, R.color.selected_item_color), PorterDuff.Mode.SRC_IN)
-                binding.checkImage.visibility = View.VISIBLE
+            val drawable = binding.friendImage.drawable
+            if (drawable is LayerDrawable) {
+                val fillDrawable = drawable.findDrawableByLayerId(R.id.fill)
+                if (friend.isSelected) {
+                    fillDrawable.setColorFilter(ContextCompat.getColor(binding.root.context, R.color.colorPrimary), PorterDuff.Mode.SRC_IN)
+                    binding.checkImage.visibility = View.VISIBLE
+                } else {
+                    fillDrawable.setColorFilter(ContextCompat.getColor(binding.root.context, android.R.color.transparent), PorterDuff.Mode.SRC_IN)
+                    binding.checkImage.visibility = View.GONE
+                }
+            }
+
+            if (friend.isFavorite) {
+                binding.favoriteIcon.setColorFilter(ContextCompat.getColor(binding.root.context, android.R.color.holo_orange_light), PorterDuff.Mode.SRC_IN)
             } else {
-                binding.friendImage.setColorFilter(ContextCompat.getColor(binding.root.context, android.R.color.white), PorterDuff.Mode.SRC_IN)
-                binding.checkImage.visibility = View.GONE
+                binding.favoriteIcon.clearColorFilter()
+                binding.favoriteIcon.setImageResource(R.drawable.ic_star)
             }
         }
     }
